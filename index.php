bc3d49d00253d35142362d47621fa76920e397bc
me8686 <mmdeb8686@gmail.com>
2025-01-12 08:31:39 +0330

    Initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>فروشگاه موبایل</title>
+    <link href="siteghaleb.css" rel="stylesheet">
+    <link rel="stylesheet" href="bootstrap.min.css">
+</head>
+<body>
+    <?php include('header.php'); ?>
+    
+    <main class="container my-5">
+    <aside class="bg-light p-3 rounded text-center">
+            <h3>به سایت فروشگاه موبایل ما خوش امدید</h3>
+            <p>امید واریم خرید خوبی را تجربه کنید</p>
+            <p>ما با به اشتراک گذاری مشخصات گوشی ها شما به را کمک میکنیم</p>
+        </aside>
+    </main>
+    
+    <?php include('footer.php'); ?>
+    
+    <link href="siteghaleb.css" rel="stylesheet">
+    <script src="bootstrap.bundle.min.js"></script>
+</body>
+</html>
